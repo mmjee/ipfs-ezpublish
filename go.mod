63ba17f4444d47aed3959d9212ac5f091f3a9953
root <root@git.maharshi.ninja>
2022-10-20 06:30:34 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,39 @@
+module ipfs-ezpublish
+
+go 1.19
+
+require (
+	github.com/BurntSushi/toml v1.0.0
+	github.com/ipfs/go-ipfs-api v0.3.0
+	github.com/jeandeaual/go-locale v0.0.0-20220711133428-7de61946b173
+	github.com/nicksnyder/go-i18n/v2 v2.2.1-0.20220629175230-57bd175f4eca
+	golang.org/x/text v0.3.7
+)
+
+require (
+	github.com/btcsuite/btcd v0.20.1-beta // indirect
+	github.com/crackcomm/go-gitignore v0.0.0-20170627025303-887ab5e44cc3 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/ipfs/go-cid v0.0.7 // indirect
+	github.com/ipfs/go-ipfs-files v0.0.9 // indirect
+	github.com/libp2p/go-buffer-pool v0.0.2 // indirect
+	github.com/libp2p/go-flow-metrics v0.0.3 // indirect
+	github.com/libp2p/go-libp2p-core v0.6.1 // indirect
+	github.com/libp2p/go-openssl v0.0.7 // indirect
+	github.com/minio/blake2b-simd v0.0.0-20160723061019-3f5f724cb5b1 // indirect
+	github.com/minio/sha256-simd v0.1.1 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/multiformats/go-base32 v0.0.3 // indirect
+	github.com/multiformats/go-base36 v0.1.0 // indirect
+	github.com/multiformats/go-multiaddr v0.3.0 // indirect
+	github.com/multiformats/go-multibase v0.0.3 // indirect
+	github.com/multiformats/go-multihash v0.0.14 // indirect
+	github.com/multiformats/go-varint v0.0.6 // indirect
+	github.com/spacemonkeygo/spacelog v0.0.0-20180420211403-2296661a0572 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/whyrusleeping/tar-utils v0.0.0-20180509141711-8c6c8ba81d5c // indirect
+	go.opencensus.io v0.22.4 // indirect
+	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
+	golang.org/x/sys v0.0.0-20211013075003-97ac67df715c // indirect
+)
---
+++ main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"embed"
+	"errors"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/BurntSushi/toml"
+	ipfs "github.com/ipfs/go-ipfs-api"
+	"github.com/jeandeaual/go-locale"
+	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"golang.org/x/text/language"
+)
+
+//go:embed translations/*.toml
+var translations embed.FS
+
+func printOptError(localizer *i18n.Localizer, err error) {
+	_, _ = fmt.Fprintln(os.Stderr, localizer.MustLocalize(&i18n.LocalizeConfig{
+		MessageID: "Messages.IgnoredError",
+		TemplateData: map[string]string{
+			"Err": err.Error(),
+		},
+	}))
+}
+
+func main() {
+	userLocales, err := locale.GetLocales()
+	if err != nil {
+		panic(err)
+	}
+	bundle := i18n.NewBundle(language.BritishEnglish)
+	bundle.RegisterUnmarshalFunc("toml", toml.Unmarshal)
+	_, _ = bundle.LoadMessageFileFS(translations, "translations/en.toml")
+	_, _ = bundle.LoadMessageFileFS(translations, "translations/bn.toml")
+	localizer := i18n.NewLocalizer(bundle, userLocales...)
+
+	var certFile, certKey, shellURL, ipnsKey, dirToUpload string
+	flag.StringVar(&certFile, "cert-file", "cert.crt", localizer.MustLocalize(&i18n.LocalizeConfig{
+		MessageID: "Arguments.CertificatePath",
+	}))
+	flag.StringVar(&certKey, "private-key", "cert.key", localizer.MustLocalize(&i18n.LocalizeConfig{
+		MessageID: "Arguments.PrivateKey",
+	}))
+	flag.StringVar(&shellURL, "shell-url", "https://localhost", localizer.MustLocalize(&i18n.LocalizeConfig{
+		MessageID: "Arguments.ShellURL",
+	}))
+	flag.StringVar(&ipnsKey, "ipns-key", "", localizer.MustLocalize(&i18n.LocalizeConfig{
+		MessageID: "Arguments.IPNSKey",
+	}))
+	flag.StringVar(&dirToUpload, "target", "dist", localizer.MustLocalize(&i18n.LocalizeConfig{
+		MessageID: "Arguments.Target",
+	}))
+	flag.Parse()
+
+	cert, err := tls.LoadX509KeyPair(certFile, certKey)
+	if err != nil {
+		panic(err)
+	}
+
+	httpClient := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				Certificates: []tls.Certificate{cert},
+			},
+		},
+	}
+	shell := ipfs.NewShellWithClient(shellURL, httpClient)
+
+	id, err := shell.ID()
+	if err != nil {
+		panic(err)
+	}
+	_, _ = fmt.Fprintln(os.Stderr, localizer.MustLocalize(&i18n.LocalizeConfig{
+		MessageID: "Messages.PublishMsg",
+		TemplateData: map[string]string{
+			"ID": id.ID,
+		},
+	}))
+
+	var keyId string
+	{
+		keyList, err := shell.KeyList(context.TODO())
+		if err != nil {
+			panic(err)
+		}
+		for _, k := range keyList {
+			if k.Name == ipnsKey {
+				keyId = k.Id
+			}
+		}
+		if len(keyId) == 0 {
+			panic(errors.New(localizer.MustLocalize(&i18n.LocalizeConfig{
+				MessageID: "Messages.CouldntFindKeyID",
+			})))
+		}
+	}
+
+	// We have to clean up previous publishes
+	oldCID, err := shell.Resolve(keyId)
+	if err != nil {
+		printOptError(localizer, err)
+		goto cleanedUp
+	}
+	_, _ = fmt.Fprintln(os.Stderr, localizer.MustLocalize(&i18n.LocalizeConfig{
+		MessageID: "Messages.OldCID",
+		TemplateData: map[string]string{
+			"CID": oldCID,
+		},
+	}))
+	err = shell.Unpin(oldCID)
+	if err != nil {
+		printOptError(localizer, err)
+		goto cleanedUp
+	}
+	err = shell.FilesRm(context.TODO(), oldCID, true)
+	if err != nil {
+		printOptError(localizer, err)
+		goto cleanedUp
+	}
+
+cleanedUp:
+	rootCID, err := shell.AddDir(dirToUpload)
+	if err != nil {
+		panic(err)
+	}
+	err = shell.Pin(rootCID)
+	if err != nil {
+		panic(err)
+	}
+	_, err = shell.PublishWithDetails(rootCID, keyId, 24*time.Hour, 24*time.Hour, true)
+	if err != nil {
+		panic(err)
+	}
+	_, _ = fmt.Fprintln(os.Stderr, localizer.MustLocalize(&i18n.LocalizeConfig{
+		MessageID: "Messages.Published",
+		TemplateData: map[string]string{
+			"CID": rootCID,
+			"Key": keyId,
+		},
+	}))
+}
